99292a278c2213cba01427caeb1b2139ff1028a1
Andrey Izotov <faradaytrs@gmail.com>
2024-09-13 17:55:31 +0800

    refactoring

--- src/diff.ts
+++ src/diff.ts
@@ -1,12 +1,36 @@
+import { readFile } from "fs/promises";
 import { octokit } from "./octokit.js";
+import { PullRequestEvent } from "@octokit/webhooks-types";
 
 export async function getDiff(owner: string, repo: string, pull_number: number): Promise<string | null> {
-    const response = await octokit.pulls.get({
-        owner,
-        repo,
-        pull_number,
-        mediaType: { format: "diff" },
-    });
-    // @ts-expect-error - response.data is a string
-    return response.data;
+    const eventData: PullRequestEvent = JSON.parse(await readFile(process.env.GITHUB_EVENT_PATH ?? "", "utf8"));
+
+    if (eventData.action === "opened") {
+        const response = await octokit.pulls.get({
+            owner,
+            repo,
+            pull_number,
+            mediaType: { format: "diff" },
+        });
+        console.log("pull request diff", response.data);
+        return response.data as unknown as string;
+    } else if (eventData.action === "synchronize") {
+        const newBaseSha = eventData.before;
+        const newHeadSha = eventData.after;
+
+        const response = await octokit.repos.compareCommits({
+            headers: {
+                accept: "application/vnd.github.v3.diff",
+            },
+            owner,
+            repo,
+            base: newBaseSha,
+            head: newHeadSha,
+        });
+
+        return String(response.data);
+    } else {
+        console.log("Unsupported event:", process.env.GITHUB_EVENT_NAME);
+        return null;
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -26,35 +26,8 @@ const analyzeCode = async (
 
 async function main() {
     const prDetails = await getPRDetails();
-    let diff: string | null;
-    const eventData = JSON.parse(readFileSync(process.env.GITHUB_EVENT_PATH ?? "", "utf8"));
 
-    if (eventData.action === "opened") {
-        diff = await getDiff(prDetails.owner, prDetails.repo, prDetails.pull_number);
-    } else if (eventData.action === "synchronize") {
-        const newBaseSha = eventData.before;
-        const newHeadSha = eventData.after;
-
-        const response = await octokit.repos.compareCommits({
-            headers: {
-                accept: "application/vnd.github.v3.diff",
-            },
-            owner: prDetails.owner,
-            repo: prDetails.repo,
-            base: newBaseSha,
-            head: newHeadSha,
-        });
-
-        diff = String(response.data);
-    } else {
-        console.log("Unsupported event:", process.env.GITHUB_EVENT_NAME);
-        return;
-    }
-
-    if (!diff) {
-        console.log("No diff found");
-        return;
-    }
+    const diff = await getDiff(prDetails.owner, prDetails.repo, prDetails.pull_number);
 
     console.log("diff", diff);
 
@@ -69,10 +42,6 @@ async function main() {
     });
 
     const comments = await analyzeCode(filteredDiff, prDetails);
-    console.log("owner", prDetails.owner);
-    console.log("repo", prDetails.repo);
-    console.log("pull_number", prDetails.pull_number);
-    console.log("Comments:", comments);
     if (comments.length > 0) {
         await createReviewComment(prDetails.owner, prDetails.repo, prDetails.pull_number, comments);
     }
